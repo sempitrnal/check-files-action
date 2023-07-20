d442f5949c71092c9b4e279a08f7c58d8b812113
Reynald Sampelo <reynalsampelo@gmail.com>
2023-07-20 17:45:03 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const core = require("@actions/core");
+const github = require("@actions/github")(async () => {
+	try {
+		core.notice("Calling our action");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+})();
